6a70e3c052d00c77c85da546429db973f5f9e140
aSavagePancake <blackthesoul@googlemail.com>
2021-06-14 14:01:39 +0100

    moved to different versioning scheme, unified version naming for both MiTL & TimerService

--- MiTL/Properties/AssemblyInfo.cs
+++ MiTL/Properties/AssemblyInfo.cs
@@ -48,4 +48,4 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("0.9.3.0")]
+[assembly: AssemblyFileVersion("1.0.2.0614")]
