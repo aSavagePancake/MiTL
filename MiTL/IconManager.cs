31b164dbf54cef68f6ada68d6aa34a6ab0890e57
aSavagePancake <blackthesoul@googlemail.com>
2021-07-29 17:07:30 +0100

    Fixed Quicklaunch extracted executable icons being saved as a low-color image - All icons extracted from .exe's for Quicklaunch feature are now stored as full color images

--- MiTL/IconManager.cs
+++ MiTL/IconManager.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Drawing;
 using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
 
 namespace MiTL
 {
@@ -11,19 +13,25 @@ namespace MiTL
             string iconPath = Environment.CurrentDirectory + @"\ql_icons\";
             string iconFile = "ql" + quickLaunchTile + ".ico";
 
+            if (!Directory.Exists(iconPath))
+            {
+                Directory.CreateDirectory(iconPath);
+            }
+
             Icon exeIcon = ExtractIconFromFilePath(executablePath);
 
             if (exeIcon != null)
             {
-                if (!Directory.Exists(iconPath))
-                {
-                    Directory.CreateDirectory(iconPath);
-                }
-                using (FileStream stream = new FileStream(iconPath + iconFile, FileMode.Create))
-                {
-                    exeIcon.Save(stream);
-                    exeIcon.Dispose();
-                }
+                Bitmap icon = exeIcon.ToBitmap();
+
+                IntPtr hBitmap = icon.GetHbitmap();
+                System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                     hBitmap, IntPtr.Zero, Int32Rect.Empty,
+                     BitmapSizeOptions.FromEmptyOptions());
+
+                icon.Save(iconPath + iconFile);
+                icon.Dispose();
+                exeIcon.Dispose();
             }
         }
 
